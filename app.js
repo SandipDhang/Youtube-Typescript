fbcf9e350be029201e3f03c051425d433384ac66
CodewaveSandy <sandy@codewave.com>
2022-12-31 13:02:21 +0530

    Day 1 Introduction

---
+++ app.js
@@ -0,0 +1,5 @@
+function getTotal(val1, val2) {
+    return val1 + val2;
+}
+var result = getTotal(10, 20);
+console.log(result);
---
+++ app.ts
@@ -0,0 +1,6 @@
+function getTotal(val1: number, val2: number) {
+  return val1 + val2;
+}
+
+const result = getTotal(10, 20);
+console.log(result);
