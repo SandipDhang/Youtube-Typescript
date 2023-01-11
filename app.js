33fe66dc1c45d39a6d9ebe6a004c5c8a0f291957
CodewaveSandy <sandy@codewave.com>
2023-01-11 20:22:18 +0530

    Modifiers in class

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
-function getTotal(val1, val2) {
-    return val1 + val2;
-}
-var result = getTotal(10, 20);
-console.log(result);
+"use strict";
+var Car = /** @class */ (function () {
+    // model: string;
+    // readonly brand: string = "Mahindra";
+    // private colorVariant: string[] = [];
+    function Car(model, brand, colorVariant) {
+        this.model = model;
+        this.brand = brand;
+        this.colorVariant = colorVariant;
+        // this.model = mdl;
+        // this.brand = brand;
+        // this.colorVariant = colorVariant;
+    }
+    Car.prototype.getDetails = function () {
+        console.log(this);
+    };
+    Car.prototype.addColorVariant = function (value) {
+        this.colorVariant.push(value);
+    };
+    return Car;
+}());
+var xuvCar = new Car("XUV", "Mahindra", ["black"]);
+// xuvCar.addColorVariant("black");
+xuvCar.addColorVariant("white");
+// xuvCar.colorVariant[2] = "red";
+// xuvCar.brand = "TATA";
+console.log(xuvCar);
--- app.ts
+++ app.ts
@@ -1,6 +1,33 @@
-function getTotal(val1: number, val2: number) {
-  return val1 + val2;
+class Car {
+  // model: string;
+  // readonly brand: string = "Mahindra";
+  // private colorVariant: string[] = [];
+
+  constructor(
+    public model: string,
+    readonly brand: string,
+    private colorVariant: string[]
+  ) {
+    // this.model = mdl;
+    // this.brand = brand;
+    // this.colorVariant = colorVariant;
+  }
+
+  getDetails(this: Car) {
+    console.log(this);
+  }
+
+  addColorVariant(value: string) {
+    this.colorVariant.push(value);
+  }
 }
 
-const result = getTotal(10, 20);
-console.log(result);
+const xuvCar = new Car("XUV", "Mahindra", ["black"]);
+// xuvCar.addColorVariant("black");
+xuvCar.addColorVariant("white");
+
+// xuvCar.colorVariant[2] = "red";
+
+// xuvCar.brand = "TATA";
+
+console.log(xuvCar);
