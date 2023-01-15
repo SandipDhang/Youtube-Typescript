5167dcef0608de0199968742168cf5e66dc65bb1
CodewaveSandy <sandy@codewave.com>
2023-01-15 19:26:26 +0530

    interfaces

--- app.js
+++ app.js
@@ -1,65 +1,15 @@
 "use strict";
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-var Car = /** @class */ (function () {
-    function Car(model, brand) {
-        this.model = model;
-        this.brand = brand;
+var User = /** @class */ (function () {
+    function User(name, email, phNo) {
+        this.name = name;
+        this.email = email;
+        this.phNo = phNo;
     }
-    Car.log = function (value) {
-        console.log(value);
-    };
-    Car.prototype.getDetails = function () {
+    User.prototype.getInformation = function () {
         console.log(this);
     };
-    Car.mfgYear = 2023;
-    return Car;
+    return User;
 }());
-var Ecar = /** @class */ (function (_super) {
-    __extends(Ecar, _super);
-    function Ecar(mdl, brand, chrgTym) {
-        var _this = _super.call(this, mdl, brand) || this;
-        _this.chargingTime = chrgTym;
-        return _this;
-    }
-    Object.defineProperty(Ecar.prototype, "getChargingTime", {
-        get: function () {
-            return this.chargingTime;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Object.defineProperty(Ecar.prototype, "setChargingTime", {
-        set: function (time) {
-            this.chargingTime = time;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Ecar.prototype.getDetails = function () {
-        console.log("This is from SUBCLASS Ecar", this);
-    };
-    Ecar.prototype.buildCar = function () {
-        console.log("Building an Electric car");
-    };
-    return Ecar;
-}(Car));
-var xuvCar = new Ecar("XUV", "Mahindra", 30);
-xuvCar.getDetails();
-xuvCar.buildCar();
-xuvCar.setChargingTime = 40;
-console.log(Car.mfgYear);
-Car.log("ANYVALUE");
+var user1 = new User("SD", "asd@asd.com", "3456789");
+// const user1 = new User("SD");
+user1.getInformation();
--- app.ts
+++ app.ts
@@ -1,43 +1,20 @@
-abstract class Car {
-  static mfgYear = 2023;
-  static log(value: string) {
-    console.log(value);
-  }
-  constructor(public model: string, readonly brand: string) {}
-  getDetails(this: Car) {
+interface Contact {
+  email: string;
+  phNo: string;
+}
+interface Individual extends Contact {
+  name: string;
+}
+
+class User implements Individual {
+  constructor(public name: string, public email: string, public phNo: string) {}
+
+  getInformation(this: User) {
     console.log(this);
   }
-
-  abstract buildCar(): void;
-}
-class Ecar extends Car {
-  private chargingTime: number;
-  constructor(mdl: string, brand: string, chrgTym: number) {
-    super(mdl, brand);
-    this.chargingTime = chrgTym;
-  }
-
-  get getChargingTime() {
-    return this.chargingTime;
-  }
-
-  set setChargingTime(time: number) {
-    this.chargingTime = time;
-  }
-
-  getDetails(this: Car) {
-    console.log("This is from SUBCLASS Ecar", this);
-  }
-
-  buildCar() {
-    console.log("Building an Electric car");
-  }
 }
 
-const xuvCar = new Ecar("XUV", "Mahindra", 30);
-xuvCar.getDetails();
-xuvCar.buildCar();
-xuvCar.setChargingTime = 40;
-console.log(Car.mfgYear);
+const user1 = new User("SD", "asd@asd.com", "3456789");
+// const user1 = new User("SD");
 
-Car.log("ANYVALUE");
+user1.getInformation();
